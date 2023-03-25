9559b000fc07d97c94049fa4b6ed6f48998bfa9e
unknown <madiha.shaikh@crownstack.com>
2023-03-25 23:21:35 +0530

    integrate backend and client

---
+++ routes/transactions.js
@@ -0,0 +1,59 @@
+const express = require('express');
+const router = express.Router();
+const db = require('../config/db');
+
+const getTransactions = (req, res) => {
+    let sql = `SELECT * FROM transactions`;
+    db.query(sql, (err, results) => {
+        if(err) throw err;
+        res.json({msg: 'Transactions fetched...', transactions: results});
+    })
+}
+
+const addTransactions = (req, res) => {
+    if(req.body.text && req.body.amount) {
+        let query = `INSERT INTO transactions
+            (text, amount) VALUES (?, ?);`;
+        let text = req.body.text;
+        let amount = req.body.amount;
+        db.query(query, [text,
+        amount], (err, rows) => {
+            if (err) throw err;
+            console.log("Row inserted with id = "
+                + rows.insertId);
+        });
+
+        res.status(201).json({
+            msg: 'Transaction added',
+            transaction: {text, amount}
+        })
+    }
+    else {
+        res.status(400).json({
+            Error: 'Either text or amount is missing!!'
+        })
+    }
+}
+
+const deleteTransactions = (req, res) => {
+    // res.send('Delete Transactions');
+    let id = req.params.id;
+    let query = `DELETE FROM transactions WHERE id=${id}`;
+    db.query(query, (err) => {
+        if(err) throw err;
+        else {
+            res.json({'msg': 'Deleted Transaction'});
+        }
+    });
+}
+
+router
+    .route('/')
+    .get(getTransactions)
+    .post(addTransactions);
+
+router
+    .route('/:id')
+    .delete(deleteTransactions);
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const colors = require('colors');
+const transactions = require('./routes/transactions');
+const db = require('./config/db');
+
+const app = express();
+
+// connection
+db.connect(function(err) {
+    if (err) throw err;
+    console.log("Database connected!");
+    // var sql = "CREATE TABLE transactions (id INT AUTO_INCREMENT PRIMARY KEY, text VARCHAR(255), amount INT)";
+    // db.query(sql, function (err, result) {
+    //   if (err) throw err;
+    //   console.log("Table created");
+    // });
+    // var sql = "DROP TABLE";
+    // db.query(sql, function (err, result) {
+    //   if (err) throw err;
+    //   console.log("Table dropped");
+    // });
+});
+
+// app.get('/createDB', (req, res) => {
+//     const sql = 'CREATE DATABASE expenseTrackerDB';
+//     db.query(sql, (err, result) => {
+//         if(err) throw err;
+//         console.log('Result', result);
+//         res.end('Database created');
+//     })
+// });
+
+// con.connect(function(err) {
+//     if (err) throw err;
+//     console.log("Connected!");
+//     var sql = "CREATE TABLE customers (name VARCHAR(255), address VARCHAR(255))";
+//     con.query(sql, function (err, result) {
+//       if (err) throw err;
+//       console.log("Table created");
+//     });
+//   });
+
+dotenv.config({path: './config/config.env'}); 
+
+// it allows us to use body parser - ex, req.body.any_property
+app.use(express.json());
+
+const PORT = process.env.PORT || 5000;
+
+// app.get('/api/v1/transactions', (req, res) => {
+//     console.log("hello");
+// });
+ 
+app.use('/api/v1/transactions', transactions); 
+
+// app.use('/api/v1/transactions', (req, res) => {
+//     res.send('Get Transactions');
+// }); 
+
+app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} on ${PORT}`.yellow));
